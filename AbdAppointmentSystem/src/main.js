570a1336ac6816335fae496c9237baac46d1fb4b
Hongyu Yan <133737661+Markyan04@users.noreply.github.com>
2024-04-01 17:06:13 +0800

    初始化

--- AbdAppointmentSystem/src/main.js
+++ AbdAppointmentSystem/src/main.js
@@ -1,11 +1,22 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import App from './App.vue'
 import router from './router'
+
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+
+axios.defaults.baseURL = "/api/v1.1"
 
 const app = createApp(App)
 
 app.use(router)
+app.use(ElementPlus)
+app.use(VueAxios, axios)
 
 app.mount('#app')
--- AbdAppointmentSystem/vite.config.js
+++ AbdAppointmentSystem/vite.config.js
@@ -5,6 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+      }
+    },
+  },
   plugins: [
     vue(),
   ],
